629fba43cea34bb9878d1b83786480c0860d4c6e
Jolterix20 <jayeshvbane@gmail.com>
2022-12-10 23:21:58 +0530

    frontend complete

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Importing libs
+const path = require('path')
 const express = require('express')
 const dotenv = require('dotenv').config()
 const port = process.env.PORT || 5000
@@ -12,6 +13,9 @@ const app = express()
 // Enable body parser for reading forms
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')))
 
 // Using routes
 app.use('/openai', openaiRoutes)
